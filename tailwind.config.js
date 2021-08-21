328600bf94db02df0154f3ed0707fca50f9c5d01
Akshay kanojia <akanojia.47@gmail.com>
2021-08-21 23:09:15 +0530

    change in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer')
-  ],
   purge: ['./src/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
